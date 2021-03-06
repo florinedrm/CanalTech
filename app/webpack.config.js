0245948d5f99c16e33cee1d80fca55eb02011699
florinedrm <florine.drm@gmail.com>
2021-03-06 18:08:13 +0100

    init webpack / homepage twig

---
+++ app/webpack.config.js
@@ -0,0 +1,39 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+
+    .splitEntryChunks()
+
+    .enableSingleRuntimeChunk()
+
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    .enableSassLoader()
+
+    .enableIntegrityHashes(Encore.isProduction())
+
+;
+
+module.exports = Encore.getWebpackConfig();
